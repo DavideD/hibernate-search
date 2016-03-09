b43de85992a8b96fd47d6e1652c82fb570c2296b
Davide D'Alto <davide@hibernate.org>
2016-03-09 16:22:50 +0000

    HSEARCH-2108 Add new interfaces and classes for remote and lucene analyzers

---
+++ engine/src/main/java/org/hibernate/search/analyzer/impl/RemoteAnalyzerReference.java
@@ -0,0 +1,43 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.analyzer.impl;
+
+/**
+ * An analyzer defined on the backend.
+ *
+ * @author Davide D'Alto
+ */
+public final class RemoteAnalyzerReference implements AnalyzerReference {
+
+	private String name;
+
+	public RemoteAnalyzerReference(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void close() {
+	}
+
+	@Override
+	public <T extends AnalyzerReference> boolean is(Class<T> analyzerType) {
+		return RemoteAnalyzerReference.class.isAssignableFrom( analyzerType );
+	}
+
+	@Override
+	public <T extends AnalyzerReference> T unwrap(Class<T> analyzerType) {
+		return (T) this;
+	}
+}
